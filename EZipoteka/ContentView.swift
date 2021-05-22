f35a32c5a5160399691daf1f3d2055e1be0e1d65
Vladislav Nikolaev <yavl98@mail.ru>
2021-05-22 19:23:05 +0900

    Initial Commit

---
+++ EZipoteka/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EZipoteka
+//
+//  Created by Vladislav Nikolaev on 22.05.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EZipoteka/EZipotekaApp.swift
@@ -0,0 +1,17 @@
+//
+//  EZipotekaApp.swift
+//  EZipoteka
+//
+//  Created by Vladislav Nikolaev on 22.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct EZipotekaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
